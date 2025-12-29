3931d60be0d89adf26f405049095d5f103189a68
ReDO1008 <alfredo.rodrigo@binus.ac.id>
2025-12-29 17:18:40 +0700

    Test

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,19 +2,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   
+
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-       
+
     }
 
     void FixedUpdate()
     {
-        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Heart"))
+        {
+            Debug.Log("+15 Hp");
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("Memory"))
+        {
+            Debug.Log("Memory Fragments Collected");
+            Destroy(other.gameObject);
+        }
     }
 }
