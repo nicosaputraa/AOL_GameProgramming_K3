480b167585e2ec6ba8fc14e0f26e100149ff7dbd
ReDO1008 <alfredo.rodrigo@binus.ac.id>
2025-12-29 17:44:37 +0700

    Collectible

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -15,6 +15,20 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
-        
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Heart"))
+        {
+            Destroy(other.gameObject);
+            Debug.Log("+15 HP");
+        }
+        else if (other.CompareTag("Memory"))
+        {
+            Destroy(other.gameObject);
+            Debug.Log("Memory Fragments Collected");
+        }
     }
 }
---
+++ TODO.md
@@ -0,0 +1,3 @@
+# TODO List for Memory Object Interaction
+
+- [x] Edit Assets/Script/Player.cs to add else if condition in OnTriggerEnter2D for "Memory" tagged objects, destroying the object, and logging "Memory Fragments Collected" to the console.
